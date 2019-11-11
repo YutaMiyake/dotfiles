f8a82ea2461e05e5abf0c082b9e5a737844daf56
YutaMiyake <ymiyake@nevada.unr.edu>
2019-11-12 00:19:54 +0900

    update fzf on zsh

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,19 +1,16 @@
-export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
+# Exclude those directories even if not listed in .gitignore, or if .gitignore is missing
+FD_OPTIONS="--follow --exclude .git --exclude node_modules"
 
-function tree-fzf() {
-  local SELECTED_FILE=$(tree --charset=o -f | fzf --query "$LBUFFER" | tr -d '\||`|-' | xargs echo)
+# Change behavior of fzf dialogue
+export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window='right:hidden:wrap' --bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | pbcopy)'"
 
-  if [ "$SELECTED_FILE" != "" ]; then
-    BUFFER="$EDITOR $SELECTED_FILE"
-    zle accept-line
-  fi
+# Change find backend
+# Use 'git ls-files' when inside GIT repo, or fd otherwise
+export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type l $FD_OPTIONS"
 
-  zle reset-prompt
-}
-
-zle -N tree-fzf
-bindkey "^t" tree-fzf
+# Find commands for "Ctrl+T" and "Opt+C" shortcuts
+export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
+export FZF_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
 
 function git-branch-fzf() {
   local selected_branch=$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads | perl -pne 's{^refs/heads/}{}' | fzf --query "$LBUFFER")
