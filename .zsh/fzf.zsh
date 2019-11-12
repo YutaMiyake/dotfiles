9ac84f3e13129ed29b4ccb016732c65aa401228f
YutaMiyake <ymiyake@nevada.unr.edu>
2019-11-12 13:34:04 +0900

    add hidden files in fd search

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,14 +1,9 @@
-# Exclude those directories even if not listed in .gitignore, or if .gitignore is missing
-FD_OPTIONS="--follow --exclude .git --exclude node_modules"
+FD_OPTIONS="--hidden --follow --exclude .git --exclude node_modules"
 
-# Change behavior of fzf dialogue
 export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window='right:hidden:wrap' --bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | pbcopy)'"
 
-# Change find backend
-# Use 'git ls-files' when inside GIT repo, or fd otherwise
 export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type l $FD_OPTIONS"
 
-# Find commands for "Ctrl+T" and "Opt+C" shortcuts
 export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
 export FZF_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
 
@@ -38,11 +33,6 @@ function ssh-fzf () {
 
 zle -N ssh-fzf
 bindkey '^\' ssh-fzf
-
-HISTFILE=~/.zsh_history
-HISTSIZE=1000000
-SAVEHIST=1000000
-setopt hist_ignore_all_dups
 
 function history-fzf() {
   local tac
